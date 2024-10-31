6c29e3374538059aff60fb22eac17f50de0932b3
kahsez <kahsez@users.noreply.github.com>
2024-10-31 16:35:50 +0100

    refactor: foreach items

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -6,12 +6,4 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    [Test]
-    public void Foo()
-    {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        var app = new GildedRose(items);
-        app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
-    }
 }
