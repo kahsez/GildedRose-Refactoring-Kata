67b2ff1628bf5df71ae056c08c5e62950c85f8af
kahsez <kahsez@users.noreply.github.com>
2024-10-31 20:22:51 +0100

    refactor: extracted quality parameter from clamp

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -14,30 +14,30 @@ public class Item
         {
             case "Aged Brie":
                 UpdateAgedBrie();
-                Quality = ClampMaxTo50();
+                Quality = ClampMaxTo50(Quality);
                 return;
             case "Backstage passes to a TAFKAL80ETC concert":
                 UpdateBackstagePass();        
-                Quality = ClampMaxTo50();
+                Quality = ClampMaxTo50(Quality);
                 return;
             case "Sulfuras, Hand of Ragnaros":
                 UpdateSulfuras();
                 return;
             default:
                 UpdateNormalItem();
-                Quality = ClampMinToZero();
+                Quality = ClampMinToZero(Quality);
                 break;
         }
     }
 
-    int ClampMinToZero()
+    int ClampMinToZero(int quality)
     {
-        return Math.Max(0, Quality);
+        return Math.Max(0, quality);
     }
 
-    int ClampMaxTo50()
+    int ClampMaxTo50(int quality)
     {
-        return Math.Min(50, Quality);
+        return Math.Min(50, quality);
     }
 
     void UpdateNormalItem()
